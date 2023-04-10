29746aeacfc49316d1993372bedef74c01e8f58e
Rich Plastow <rich@richplastow.com>
2023-04-10 09:53:36 +0300

    adds the ConfigDescriptor type

--- cli-config-helpers.js
+++ cli-config-helpers.js
@@ -119,4 +119,56 @@ function parseArgv(argv) {
     return { config, ignored };
 }
 
-export { parseArgv };
+/**
+ * ### Describes a value expected in a config file, `process.env` or `argv`.
+ *
+ * @typedef {object} ConfigDescriptor
+ * @property {boolean|number|string} [fallback]
+ *    An optional default value to use if the configuration file, `process.env`
+ *    and `process.argv` do not contain this variable. If `fallback` is
+ *    `undefined`, the value is mandatory.
+ * @property {'boolean'|'number'|'string'} kind
+ *    Determines the allowed type of value, `boolean`, `number`, or `string`.
+ * @property {string} [nameArgvLong]
+ *    The value's long name in the 'arguments vector', `process.argv`.
+ *    - Must start with a lowercase ASCII letter
+ *    - Must continue with at least one dash, digit or lowercase ASCII letter
+ *    - Must be no longer than 32 characters
+ *    - If `undefined`, there is no long name
+ * @property {string} [nameArgvShort]
+ *    The value's short name in the 'arguments vector', `process.argv`.
+ *    - Must be a lower or uppercase ASCII letter, or the question mark `"?"`
+ *    - Must be exactly one character long
+ *    - If `undefined`, there is no short name
+ * @property {string} [nameEnv]
+ *    The value's name in the shell environment, `process.env`.
+ *    - Must start with an uppercase ASCII letter
+ *    - May continue with an underscore, digit or uppercase ASCII letter
+ *    - Must be no longer than 32 characters
+ *    - If `undefined`, value cannot come from the shell environment
+ * @property {string} nameReturned
+ *    The property name in the dictionary object that `gatherConfig()` returns.
+ *    - Must start with an underscore or ASCII letter
+ *    - May continue with an underscore, digit or ASCII letter
+ *    - Must be no longer than 32 characters
+ * @property {string} [note]
+ *    A one-line summary of the expected value, up to 64 characters long, which
+ *    will be displayed by `generateHelp()` if not `undefined`.
+ *    - May contain any printable ASCII character, except the backslash `"\"`
+ *    - Must be between 1 and 64 characters long
+ */
+
+/**
+ * ### An empty array of `ConfigDescriptor` objects.
+ * 
+ * If no values are expected, there are no configuration descriptors. A function
+ * which takes a `configDescriptors` argument could use this as the default.
+ * 
+ * But using an empty `[]` like this is really a way to export the type, while
+ * avoiding the "File '...' is not a module. ts(2306)" error.
+ * 
+ * @type ConfigDescriptor[]
+ */
+const defaultConfigDescriptors = [];
+
+export { defaultConfigDescriptors, parseArgv };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 export { default as parseArgv } from './parse-argv.js';
+
+export { default as defaultConfigDescriptors }
+    from './types/default-config-descriptors.js';
